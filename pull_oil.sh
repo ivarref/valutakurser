e59ee7a4714eae021fc3a2e66ae229e30823d612
Ivar Refsdal <refsdal.ivar@gmail.com>
2016-03-23 16:34:19 +0100

    start generate normalized 12 mma oil data

--- pull_oil.sh
+++ pull_oil.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 DAT="$(python -c "import datetime; today = datetime.date.today(); first = today.replace(day=1); lastMonth = first - datetime.timedelta(days=1); print lastMonth.strftime('%Y-%m-01')")"
 
 curl --data "form[units]=lin&form[frequency]=Monthly&form[aggregation]=Average&form[obs_start_date]=1987-06-01&form[obs_end_date]=$DAT&form[file_format]=csv" https://research.stlouisfed.org/fred2/series/DCOILBRENTEU/downloaddata | tr -d '\r' > monthly_brent.csv
